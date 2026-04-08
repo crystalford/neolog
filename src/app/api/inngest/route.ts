b094fac4e37e20c6dad74a3bc524b7ebb34a4aef
crystalford <chrisrobtelford@gmail.com>
2026-04-08 09:34:20 -0400

    Slim down: remove legacy Inngest functions and disconnected old crypto-dependent code

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -6,10 +6,6 @@ import { synthesizeUserGraph } from '@/inngest/functions/synthesize-user-graph'
 import { triggerVoiceClone } from '@/inngest/functions/trigger-voice-clone'
 import { triggerLoraTraining } from '@/inngest/functions/trigger-lora-training'
 import { assembleClip } from '@/inngest/functions/assemble-clip'
-import { processChatSession } from '@/inngest/functions/process-chat'
-import { processCapture } from '@/inngest/functions/process-capture'
-import { processText } from '@/inngest/functions/process-text'
-import { refineSignal } from '@/inngest/functions/refine-signals'
 import { reanalyzeAllUploads } from '@/inngest/functions/reanalyze-all-uploads'
 import { synthesizeProject } from '@/inngest/functions/synthesize-project'
 import { developIdea } from '@/inngest/functions/develop-idea'
@@ -26,10 +22,6 @@ export const { GET, POST, PUT } = serve({
     triggerVoiceClone,
     triggerLoraTraining,
     assembleClip,
-    processChatSession,
-    processCapture,
-    processText,
-    refineSignal,
     reanalyzeAllUploads,
     developIdea,
   ],
